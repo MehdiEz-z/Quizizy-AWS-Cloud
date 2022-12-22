f9de57cf1c594102c40dc5ec869f4acf127a4019
Mehdi Ez-zahri <mehdiezzahriii@gmail.com>
2022-12-22 16:12:22 +0100

    Switch d'une question a l'autre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,50 @@
 //Start Quiz
-const enter_btn = document.querySelector(".enter-btn button");
+const enter_btn     = document.querySelector(".enter-btn button");
 
 //Infos Box
-const info_box = document.querySelector(".info-box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const start_btn = info_box.querySelector(".buttons .start");
+const info_box      = document.querySelector(".info-box");
+const exit_btn      = info_box.querySelector(".buttons .quit");
+const start_btn     = info_box.querySelector(".buttons .start");
 
 //Quiz Box
-const quiz_box = document.querySelector(".quiz-box");
-const next_btn = quiz_box.querySelector(".next-btn");
+const quiz_box      = document.querySelector(".quiz-box");
+const next_btn      = quiz_box.querySelector(".next-btn");
 
 //Stepper component
-const stepper = document.querySelector(".container");
-const bullet_one = stepper.querySelector("#bullet-1");
-const bullet_two = stepper.querySelector("#bullet-2");
-const bullet_tree = stepper.querySelector("#bullet-3");
-const bar_stepper = document.querySelector(":root");
+const stepper       = document.querySelector(".container");
+const bullet_one    = stepper.querySelector("#bullet-1");
+const bullet_two    = stepper.querySelector("#bullet-2");
+const bullet_tree   = stepper.querySelector("#bullet-3");
+const bar_stepper   = document.querySelector(":root");
 
 //Result Box
-const result_box = document.querySelector(".result-box");
-const quit_btn = result_box.querySelector(".buttons .quit");
-const replay_btn = result_box.querySelector(".buttons .restart");
+const result_box    = document.querySelector(".result-box");
+const quit_btn      = result_box.querySelector(".buttons .quit");
+const replay_btn    = result_box.querySelector(".buttons .restart");
 
 // Counter Of Questions
 let question_counter = 0;
 
 
-// ------------------------------------------------------------------------------//
+// ------------------------ Buttons ------------------------ //
 
 
 // Button Quizizy clicked
-enter_btn.onclick = ()=>{
+enter_btn.onclick   = ()=>{
     info_box.classList.add("active-info"); //Show info box
     stepper.classList.add("active-stepper"); //Show stepper
     bullet_one.classList.add("coloring-bullet"); //Coloring first bullet
 }
 
 // Button Exit Quiz clicked
-exit_btn.onclick = ()=>{
+exit_btn.onclick    = ()=>{
     info_box.classList.remove("active-info"); //Hide info box
     stepper.classList.remove("active-stepper"); //Hide stepper
     bullet_one.classList.remove("coloring-bullet"); //Deoloring first bullet
 }
 
 // Button Start Quiz clicked
-start_btn.onclick = ()=>{
+start_btn.onclick   = ()=>{
     bar_stepper.style.setProperty('--bar-stepper', '50%') //Progress stepper to second step
     info_box.classList.remove("active-info"); //Hide info box
     quiz_box.classList.add("active-quiz"); //Show quiz box
@@ -52,8 +52,18 @@ start_btn.onclick = ()=>{
     showQuestions(0);
 }
 
+// Button Next clicked
+next_btn.onclick    = ()=>{
+    if(question_counter < questions.length - 1){
+        question_counter ++;
+        showQuestions(question_counter);
+    }else{
+        showResult();
+    }
+}
+
 // Button Replay Quiz clicked
-replay_btn.onclick = ()=>{
+replay_btn.onclick  = ()=>{
     result_box.classList.remove("active-result"); //Hide stepper
     bar_stepper.style.setProperty('--bar-stepper', '50%') //Restart Progress stepper to first step
     quiz_box.classList.add("active-quiz"); //Show quiz box
@@ -63,51 +73,41 @@ replay_btn.onclick = ()=>{
 }
 
 // Button Quit Quiz clicked
-quit_btn.onclick = ()=>{
-    result_box.classList.remove("active-result"); //Hide stepper
-    stepper.classList.remove("active-stepper"); //Hide stepper
+quit_btn.onclick    = ()=>{
+    result_box.classList.remove("active-result") //Hide stepper
+    stepper.classList.remove("active-stepper") //Hide stepper
     bar_stepper.style.setProperty('--bar-stepper', '0%') //Restart Progress stepper to first step
-    bullet_one.classList.remove("coloring-bullet"); //Decoloring first bullet
-    bullet_two.classList.remove("coloring-bullet"); //Decoloring second bullet
-    bullet_tree.classList.remove("coloring-bullet"); //Decooloring third bullet
-    question_counter = 0;
-    showQuestions(question_counter);
+    bullet_one.classList.remove("coloring-bullet") //Decoloring first bullet
+    bullet_two.classList.remove("coloring-bullet") //Decoloring second bullet
+    bullet_tree.classList.remove("coloring-bullet") //Decooloring third bullet
+    question_counter = 0
+    showQuestions(question_counter)
 }
 
 
-next_btn.onclick = ()=>{
-    if(question_counter < questions.length - 1){
-        question_counter ++;
-        showQuestions(question_counter);
-    }else{
-        showResult();
-    }
-}
-
-
-// ------------------------------------------------------------------------------//
+// ------------------------ Functions ------------------------ //
 
 
 //Geting Questions and Responses From Array
 function showQuestions(index){
     //Get Question
-    const question_text = document.querySelector(".question-text");
-    let question_tag = `<span>${questions[index].suite +".  "+ questions[index].question}</span>`
-    question_text.innerHTML = question_tag;
+    const question_text     = quiz_box.querySelector(".question-text")
+    let question_tag        = `<span>${questions[index].suite +".  "+ questions[index].question}</span>`
+    question_text.innerHTML = question_tag
 
     //Get Response
-    const option_list = document.querySelector(".option-list");
-    let option_tag = `<div class="option"><span>${questions[index].options[0]}</span></div>
-                        <div class="option"><span>${questions[index].options[1]}</span></div>
-                        <div class="option"><span>${questions[index].options[2]}</span></div>
-                        <div class="option"><span>${questions[index].options[3]}</span></div> `
-    option_list.innerHTML = option_tag;
+    const option_list       = quiz_box.querySelector(".option-list")
+    let option_tag          = `<div class="option"><span>${questions[index].options[0]}</span></div>
+                                <div class="option"><span>${questions[index].options[1]}</span></div>
+                                <div class="option"><span>${questions[index].options[2]}</span></div>
+                                <div class="option"><span>${questions[index].options[3]}</span></div> `
+    option_list.innerHTML   = option_tag
 }
 
 //Show Result Box
 function showResult(){
     bar_stepper.style.setProperty('--bar-stepper', '95%') //Progress stepper to third step
-    quiz_box.classList.remove("active-quiz"); //Hide quiz box
+    quiz_box.classList.remove("active-quiz") //Hide quiz box
     result_box.classList.add("active-result") //Show result
-    bullet_tree.classList.add("coloring-bullet"); //Coloring third bullet
+    bullet_tree.classList.add("coloring-bullet") //Coloring third bullet
 }
